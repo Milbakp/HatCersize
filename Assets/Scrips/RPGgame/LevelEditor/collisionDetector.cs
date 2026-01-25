774051e943d37281b9f472794f88503c51dce152
Luqman1221102096 <luqmanirfan618@gmail.com>
2026-01-25 14:08:07 +0800

    Revert "Fail"
    This reverts commit 24d1f518081b3eff873591e911f8b65afd4604e9.

--- Assets/Scrips/RPGgame/LevelEditor/collisionDetector.cs
+++ Assets/Scrips/RPGgame/LevelEditor/collisionDetector.cs
@@ -4,16 +4,12 @@ public class collisionDetector : MonoBehaviour
 {
     public bool isColliding;
     public bool isOnMap;
-    private Collider triggerCollider;
-    public bool isFullyInside;
     void Start()
     {
-        triggerCollider = GetComponent<Collider>();
-        triggerCollider.isTrigger = true;
+        Collider collider = GetComponent<Collider>();
+        collider.isTrigger = true;
         isColliding = false;
         isOnMap = false;
-        isFullyInside = false;
-
     }
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Map") || other.gameObject.CompareTag("Tile"))
@@ -24,25 +20,13 @@ public class collisionDetector : MonoBehaviour
         Debug.Log("Collision");
         isColliding = true;
     }
+
     void OnTriggerExit(Collider other) {
         if (other.gameObject.CompareTag("Map") || other.gameObject.CompareTag("Tile"))
         {
             isOnMap = false;
         }
         isColliding = false;
-    }
-
-    public bool IsFullyInside(Collider container, Collider target)
-    {
-        // Get the bounding boxes for both
-        Bounds containerBounds = container.bounds;
-        Bounds targetBounds = target.bounds;
-
-        bool xInside = targetBounds.min.x >= containerBounds.min.x && targetBounds.max.x <= containerBounds.max.x;
-        bool zInside = targetBounds.min.z >= containerBounds.min.z && targetBounds.max.z <= containerBounds.max.z;
-
-        // Check if the container's bounds completely encapsulate the target's bounds
-        return xInside && zInside;
     }
 
 }
